ef0ef2d3c9e896c9a93b87fcc82889e3acaebdc1
Daniel Hawn <danieljhawn@gmail.com>
2020-08-31 18:16:43 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/layout/Header';
+import Header from './components/layout/Header.js';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
